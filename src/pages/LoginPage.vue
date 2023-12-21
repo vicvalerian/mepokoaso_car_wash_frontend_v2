ae87d6a361155aaf66fd374c9df11aaa8217e9b1
Vicky Valerian <vickyvalerian27@gmail.com>
2023-12-21 15:54:57 +0800

    ketinggalan

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,175 @@
 <template>
-	<div class="section-login">
-		<v-card class="mx-auto" width="400" prepend-icon="mdi-home">
-			<template v-slot:title> This is a title </template>
-
-			<v-card-text> This is content </v-card-text>
-		</v-card>
-	</div>
+	<v-app>
+		<div class="section-login">
+			<v-card class="mx-auto login-card" width="1000">
+				<div class="login">
+					<div class="form-container">
+						<v-card-title class="login-title">
+							<img src="../assets/icon.png" alt="Logo Mepokoaso Car Wash" class="login-logo" />
+						</v-card-title>
+						<v-card-text>
+							<h1 class="login-heading-primary">Selamat datang kembali</h1>
+							<h3 class="login-heading-tertiary">Silahkan masuk untuk memulai sesi anda!</h3>
+							<v-form>
+								<v-text-field
+									:disabled="loading ? true : false"
+									label="Nama Pengguna"
+									rounded=""
+									variant="outlined"
+									:rules="rules"
+									color="#316291"></v-text-field>
+								<v-text-field
+									:disabled="loading ? true : false"
+									type="password"
+									label="Kata Sandi"
+									rounded=""
+									variant="outlined"
+									:rules="rules"
+									color="#316291"></v-text-field>
+								<v-btn variant="outlined" class="login-btn" :loading="loading" @click="submitLogin">
+									Masuk
+								</v-btn>
+							</v-form>
+							<p class="copyright">&copy;2023 Vicvalerian All rights reserved.</p>
+						</v-card-text>
+					</div>
+					<div class="login-img-container" role="img" aria-label="User Login in Website"></div>
+				</div>
+			</v-card>
+		</div>
+	</v-app>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			username: '',
+			password: '',
+			loading: false,
+			rules: [
+				(value) => {
+					if (value) return true;
+
+					return 'Inputan tidak boleh kosong!';
+				},
+			],
+		};
+	},
+	methods: {
+		submitLogin() {
+			this.loading = true;
+            setTimeout(() => {
+                this.loading = false;
+            }, 2500);
+		},
+	},
+};
+</script>
+
+<style scoped>
+.v-input--horizontal {
+	margin-bottom: 0.8rem;
+}
+.section-login {
+	height: 100vh;
+	background-color: #eaeff4;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.form-container {
+	padding: 1.2rem;
+}
+.login-title {
+	display: flex;
+	justify-content: flex-start;
+}
+.login-logo {
+	width: 20%;
+}
+
+.login-card {
+	box-shadow: 0 2.4rem 4.8rem rgba(0, 0, 0, 0.15);
+	border-radius: 1.8rem;
+}
+
+.login {
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	justify-items: center;
+	align-items: center;
+}
+
+.login-heading-primary,
+.login-heading-tertiary {
+	color: #333;
+	text-align: left;
+}
+
+.login-heading-primary {
+	font-weight: 700;
+	font-size: 1.4rem;
+	margin-bottom: 0.8rem;
+	letter-spacing: -0.5px;
+}
+
+.login-heading-tertiary {
+	font-weight: 400;
+	font-size: 1rem;
+	margin-bottom: 2.4rem;
+	letter-spacing: 0.01px;
+}
+
+.login-img-container {
+	width: 100%;
+	height: 100%;
+
+	background-image: linear-gradient(
+			to right bottom,
+			rgba(58, 101, 142, 0.35),
+			rgba(30, 87, 141, 0.35)
+		),
+		url('../assets/login-image.jpg');
+	background-size: cover;
+	background-position: center;
+}
+.login-img {
+	width: 80%;
+}
+.v-btn.v-btn--density-default {
+	height: auto;
+}
+
+.login-btn {
+	background-color: #316291;
+	color: #fff;
+
+	text-transform: capitalize;
+	font-weight: 700;
+	font-size: 0.8rem;
+	letter-spacing: -0.05px;
+
+	border-radius: 100px;
+	width: 100%;
+	padding-top: 0.8rem;
+	padding-bottom: 0.8rem;
+
+	transition: all 0.3s;
+}
+
+.login-btn:hover {
+	background-color: #fff;
+	color: #316291;
+}
+
+.copyright {
+	color: #777;
+	font-size: 0.8rem;
+	font-weight: 400;
+	letter-spacing: -0.05px;
+
+	margin-top: 4.8rem;
+}
+</style>
