6a51cf37ff5bed84f9fb7e1dc7b0ce6e234daca5
Vicky Valerian <vickyvalerian27@gmail.com>
2023-12-21 10:59:11 +0800

    css file scaffolding + use vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
+import vuetify from '@/plugins/vuetify';
 
 const app = createApp(App);
 
+app.use(vuetify);
 app.use(router);
 
 app.mount('#app');
