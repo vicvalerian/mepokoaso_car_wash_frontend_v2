356224bd463e4948b34fc9618655042d722e2be5
Vicky Valerian <vickyvalerian27@gmail.com>
2023-12-30 13:33:57 +0800

    ketinggalan

--- src/main.js
+++ src/main.js
@@ -2,15 +2,20 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
 import vuetify from '@/plugins/vuetify';
+import axios from 'axios';
 
 // local component import
 import BaseSnackbar from './components/ui/BaseSnackbar.vue';
+import BaseTable from './components/ui/BaseTable.vue';
 
 const app = createApp(App);
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 app.use(vuetify);
 app.use(router);
 
 app.component('base-snackbar', BaseSnackbar);
+app.component('base-table', BaseTable);
 
 app.mount('#app');
