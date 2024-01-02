534e89345ecc0dceaeceb32dea85c7389896895a
Vicky Valerian <vickyvalerian27@gmail.com>
2024-01-02 14:16:08 +0800

    jabatan module

--- src/pages/jabatan/ListJabatan.vue
+++ src/pages/jabatan/ListJabatan.vue
@@ -1,11 +1,323 @@
 <template>
 	<v-main>
 		<div class="pages--container">
-			<h1>Data Jabatan</h1>
+			<div class="page-title-container mb-4">
+				<h1 class="pages--title">Jabatan</h1>
+				<v-btn rounded="sm" elevation="0" class="primary--btn" @click="dialogAddEdit = true">
+					Tambah Data
+				</v-btn>
+			</div>
+			<v-card>
+				<base-table
+					:list="list"
+					@update:currentPage="goToPage"
+					@update:rowsPerPage="setRowPerPage"
+					@action-click="handleActionClick"
+					@xSearch="xSearch"></base-table>
+			</v-card>
+
+			<v-dialog v-model="dialogAddEdit" persistent max-width="600px">
+				<v-card>
+					<v-card-title class="dialog-confirm-title">
+						<span class="headline white--text">{{ formTitle }} Data Jabatan</span>
+					</v-card-title>
+					<v-card-text class="dialog-confirm-text">
+						<v-container v-if="formTitle == 'Tambah'">
+							<v-text-field
+								v-model="form.nama"
+								label="Nama Jabatan"
+								required
+								variant="underlined"></v-text-field>
+						</v-container>
+						<v-container v-else>
+							<v-text-field
+								v-model="form.nama"
+								label="Nama Jabatan"
+								required
+								variant="underlined"></v-text-field>
+						</v-container>
+					</v-card-text>
+					<v-card-actions>
+						<v-spacer></v-spacer>
+						<v-btn dense class="secondary--btn" variant="outlined" @click="cancelForm()"
+							>Batal</v-btn
+						>
+						<v-btn dense class="primary--btn" :loading="btnLoading" @click="submitForm()"
+							>Simpan</v-btn
+						>
+					</v-card-actions>
+				</v-card>
+			</v-dialog>
+
+			<v-dialog v-model="dialogDelete" persistent max-width="400px">
+				<v-card>
+					<v-card-title class="dialog-confirm-title">
+						<span class="headline white--text">Hapus Data Jabatan</span>
+					</v-card-title>
+					<v-card-text class="dialog-confirm-text">
+						<span>Apakah Anda yakin ingin menghapus data jabatan ini?</span>
+					</v-card-text>
+					<v-card-actions>
+						<v-spacer></v-spacer>
+						<v-btn dense class="secondary--btn" @click="dialogDelete = false">Batal</v-btn>
+						<v-btn dense class="primary--btn" :loading="btnLoading" @click="deleteData()"
+							>Hapus</v-btn
+						>
+					</v-card-actions>
+				</v-card>
+			</v-dialog>
+
+			<base-snackbar
+				v-model="snackbar.status"
+				:color="snackbar.color"
+				:message="snackbar.message"></base-snackbar>
 		</div>
 	</v-main>
 </template>
 
-<style scoped>
+<script>
+import axios from 'axios';
 
-</style>
+export default {
+	data() {
+		return {
+			userLogin: JSON.parse(localStorage.getItem('userLogin')),
+			formTitle: 'Tambah',
+			list: {
+				headers: [
+					{ title: 'No', value: 'nomor', width: '5%', type: 'number' },
+					{ title: 'Nama Jabatan', value: 'nama', sortable: true, width: '60%' },
+					{ title: 'Aksi', value: 'actions', type: 'actions', sortable: false, width: '30%' },
+				],
+				datas: [],
+				loading: true,
+				paginate: {
+					rowsPerPage: 10,
+					currentPage: 1,
+				},
+			},
+			jabatan: new FormData(),
+			editId: '',
+			deleteId: '',
+			form: {
+				uuid: '',
+				id: '',
+				nama: '',
+			},
+			dialogAddEdit: false,
+			dialogDelete: false,
+			snackbar: {
+				status: false,
+				message: '',
+				color: '',
+			},
+			btnLoading: false,
+		};
+	},
+	created() {
+		let url = `api/jabatan?per_page=${this.list.paginate.rowsPerPage}&page=${this.list.paginate.currentPage}`;
+		this.fetchDataFromServer(url);
+	},
+	methods: {
+		resetForm() {
+			this.formTitle = 'Tambah';
+			this.form.uuid = '';
+			this.form.id = '';
+			this.form.nama = '';
+		},
+		async fetchDataFromServer(url) {
+			this.list.loading = true;
+			try {
+				const headers = {
+					Authorization: `Bearer ${this.userLogin.token}`,
+				};
+
+				const response = await axios.get(url, { headers });
+				if (response.status == 200) {
+					const data = await response.data;
+
+					// this.list.datas = data.data;
+					this.list.datas = this.addActionsToData(data.data);
+
+					this.list.totalDatas = data.total;
+
+					this.list.paginate.rowsPerPage = parseInt(data.per_page);
+					this.list.paginate.total = data.total;
+					this.list.paginate.from = data.from;
+					this.list.paginate.to = data.to;
+					this.list.paginate.lastPage = data.last_page;
+					this.list.paginate.currentPage = parseInt(data.current_page);
+					this.list.paginate.firstPageUrl = data.first_page_url;
+					this.list.paginate.lastPageUrl = data.last_page_url;
+					this.list.paginate.prevPageUrl = data.prev_page_url;
+					this.list.paginate.nextPageUrl = data.next_page_url;
+					this.list.paginate.goToPage = this.goToPage;
+				}
+
+				this.list.loading = false;
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		setRowPerPage(val = 10) {
+			console.log(val);
+			let url = `api/jabatan?per_page=${val}&page=1`;
+			this.fetchDataFromServer(url);
+		},
+		goToPage(val = 0) {
+			let url = `api/jabatan?per_page=${this.list.paginate.rowsPerPage}&page=${val}`;
+			this.fetchDataFromServer(url);
+		},
+		addActionsToData(data) {
+			return data.map((item) => ({
+				...item,
+				actions: [
+					{ title: 'Ubah', icon: 'mdi-pencil' },
+					{ title: 'Hapus', icon: 'mdi-delete' },
+				],
+			}));
+		},
+		handleActionClick({ action, item }) {
+			switch (action.title) {
+				case 'Ubah':
+					this.resetForm;
+					this.formTitle = 'Ubah';
+					this.form.uuid = item.uuid;
+					this.form.id = item.id;
+					this.form.nama = item.nama;
+					this.editId = item.uuid;
+					this.dialogAddEdit = true;
+
+					break;
+
+				case 'Hapus':
+					this.deleteId = item.uuid;
+					this.dialogDelete = true;
+					break;
+
+				default:
+					break;
+			}
+		},
+		xSearch({ query }) {
+			if (query.length > 2 || query.length == 0) {
+				let url = `api/jabatan?per_page=10&page=1&keyword=${query}`;
+				this.fetchDataFromServer(url);
+			} else {
+				this.snackbar.status = true;
+				this.snackbar.color = 'red';
+				this.snackbar.message = 'Harap masukkan minimal 3 kata pencarian!';
+			}
+		},
+		cancelForm() {
+			this.resetForm();
+			this.dialogAddEdit = false;
+		},
+		submitForm() {
+			this.btnLoading = true;
+
+			if (this.formTitle == 'Tambah') {
+				this.saveData();
+			} else if (this.formTitle == 'Ubah') {
+				this.updateData();
+			}
+		},
+		async saveData() {
+			this.jabatan.append('nama', this.form.nama);
+
+			try {
+				const headers = {
+					Authorization: `Bearer ${this.userLogin.token}`,
+				};
+
+				const response = await axios.post('api/jabatan', this.jabatan, { headers });
+				if (response.status == 200) {
+					this.snackbar.message = response.data.message;
+					this.snackbar.color = 'green';
+					this.snackbar.status = true;
+					this.btnLoading = false;
+					this.dialogAddEdit = false;
+
+					//fetch new data
+					let url = `api/jabatan?per_page=${this.list.paginate.rowsPerPage}&page=${this.list.paginate.currentPage}`;
+					this.fetchDataFromServer(url);
+
+					this.resetForm();
+				}
+			} catch (error) {
+				this.snackbar.message = error.response.data.message;
+				this.snackbar.color = 'red';
+				this.snackbar.status = true;
+				this.btnLoading = false;
+				this.dialogAddEdit = false;
+				this.resetForm();
+			}
+		},
+		async updateData() {
+			var data = new FormData();
+			data.append('nama', this.form.nama);
+
+			try {
+				const headers = {
+					Authorization: `Bearer ${this.userLogin.token}`,
+				};
+
+				let editUrl = 'api/jabatan/' + this.editId;
+				const response = await axios.post(editUrl, data, { headers });
+				if (response.status == 200) {
+					this.snackbar.message = response.data.message;
+					this.snackbar.color = 'green';
+					this.snackbar.status = true;
+					this.btnLoading = false;
+					this.dialogAddEdit = false;
+
+					//fetch new data
+					let url = `api/jabatan?per_page=${this.list.paginate.rowsPerPage}&page=${this.list.paginate.currentPage}`;
+					this.fetchDataFromServer(url);
+
+					this.resetForm();
+				}
+			} catch (error) {
+				this.snackbar.message = error.response.data.message;
+				this.snackbar.color = 'red';
+				this.snackbar.status = true;
+				this.btnLoading = false;
+				this.dialogAddEdit = false;
+				this.resetForm();
+			}
+		},
+		async deleteData() {
+			this.btnLoading = true;
+
+			try {
+				const headers = {
+					Authorization: `Bearer ${this.userLogin.token}`,
+				};
+
+				let deleteUrl = 'api/jabatan/' + this.deleteId;
+				const response = await axios.delete(deleteUrl, { headers });
+				if (response.status == 200) {
+					this.snackbar.message = response.data.message;
+					this.snackbar.color = 'green';
+					this.snackbar.status = true;
+					this.btnLoading = false;
+					this.dialogDelete = false;
+
+					//fetch new data
+					let url = `api/jabatan?per_page=${this.list.paginate.rowsPerPage}&page=${this.list.paginate.currentPage}`;
+					this.fetchDataFromServer(url);
+
+					this.resetForm();
+				}
+			} catch (error) {
+				this.snackbar.message = error.response.data.message;
+				this.snackbar.color = 'red';
+				this.snackbar.status = true;
+				this.btnLoading = false;
+				this.dialogDelete = false;
+				this.resetForm();
+			}
+		},
+	},
+};
+</script>
