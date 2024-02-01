ea0bb83be41fbe97bb8fb8e168bb0309044b0155
Vicky Valerian <vickyvalerian27@gmail.com>
2024-02-01 12:37:31 +0800

    sidebar group menu

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -1,19 +1,51 @@
 <template>
 	<v-app>
 		<div class="dashboard">
-			<v-navigation-drawer :elevation="0">
+			<v-navigation-drawer :elevation="0" v-model="drawer" width="285">
 				<v-list-item style="border-bottom: 1px solid #eee">
 					<v-list-item-title class="navbar-title">Mepokoaso CarWash</v-list-item-title>
 				</v-list-item>
 				<v-divider></v-divider>
-				<div v-for="(link, i) in links" :key="i">
-					<v-list-item link :to="link.to" class="list-item-container">
-						<div class="link-item">
-							<v-icon :icon="link.icon" class="link-icon" size="small"></v-icon>
-							<v-list-item-title class="navbar-item-title">{{ link.title }}</v-list-item-title>
-						</div>
-					</v-list-item>
-				</div>
+				<v-list open-strategy="single">
+					<div v-for="(link, i) in links" :key="i">
+						<v-list-item v-if="!link.subLinks" link :to="link.to" class="list-item-container">
+							<div class="link-item">
+								<v-icon :icon="link.icon" class="link-icon" size="small"></v-icon>
+								<v-list-item-title class="navbar-item-title"> {{ link.title }}</v-list-item-title>
+							</div>
+						</v-list-item>
+						<v-list-group
+							v-else
+							:key="link.title"
+							:value="link.title"
+							class="list-item-group-container">
+							<template v-slot:activator="{ props }">
+								<v-list-item v-bind="props" class="list-item-container">
+									<div class="link-item">
+										<v-icon :icon="link.icon" class="link-icon" size="small"></v-icon>
+										<v-list-item-title class="navbar-item-title">
+											{{ link.title }}
+										</v-list-item-title>
+									</div>
+								</v-list-item>
+							</template>
+							<v-list-item
+								v-for="sublink in link.subLinks"
+								:key="sublink.title"
+								:value="sublink.title"
+								:to="sublink.to"
+								link
+								class="list-item-container">
+								<div class="link-item">
+									<v-icon :icon="sublink.icon" class="link-icon" size="small"></v-icon>
+									<v-list-item-title class="navbar-item-title">
+										{{ sublink.title }}
+									</v-list-item-title>
+								</div>
+							</v-list-item>
+						</v-list-group>
+					</div>
+				</v-list>
 			</v-navigation-drawer>
 
 			<!-- <v-app-bar :elevation="0" class="appbar">
@@ -25,6 +57,9 @@
 				</template>
 			</v-app-bar> -->
 			<v-app-bar :elevation="0" class="appbar">
+				<template v-slot:prepend>
+					<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+				</template>
 				<template v-slot:append>
 					<v-menu location="bottom" transition="slide-y-transition">
 						<template v-slot:activator="{ props }">
@@ -69,6 +104,7 @@ export default {
 	data() {
 		return {
 			userLogin: JSON.parse(localStorage.getItem('userLogin')),
+			drawer: true,
 			loggedInUser: {
 				name: '',
 				photo: '',
@@ -76,18 +112,36 @@ export default {
 			baseUrl: 'http://127.0.0.1:8000/storage/',
 			links: [
 				{ title: 'Dashboard', to: '/dashboard', icon: 'mdi-dots-grid' },
-				{ title: 'Jabatan', to: '/jabatan', icon: 'mdi-badge-account-horizontal-outline' },
-				{ title: 'Karyawan', to: '/karyawan', icon: 'mdi-account-group' },
-				{ title: 'Jenis Kendaraan', to: '/jenis-kendaraan', icon: 'mdi-car-info' },
-				{ title: 'Kendaraan', to: '/kendaraan', icon: 'mdi-car' },
-				{ title: 'Mobil Pelanggan', to: '/mobil-pelanggan', icon: 'mdi-account-key' },
-				{ title: 'Menu Kedai', to: '/menu-kedai', icon: 'mdi-book-open' },
-				{ title: 'Peminjaman Karyawan', to: '/peminjaman-karyawan', icon: 'mdi-checkbook' },
-				{ title: 'Gaji Karyawan', to: '/gaji-karyawan', icon: 'mdi-cash' },
-				{ title: 'Pengeluaran Kedai', to: '/pengeluaran-kedai', icon: 'mdi-cart-plus' },
-				{ title: 'Transaksi Kedai', to: '/transaksi-kedai', icon: 'mdi-store' },
-				{ title: 'Transaksi Pencucian', to: '/transaksi-pencucian', icon: 'mdi-car-wash' },
-				{ title: 'Pembelanjaan Harian', to: '/pembelanjaan-harian', icon: 'mdi-cart-variant' },
+				{
+					title: 'Transaksi',
+					icon: 'mdi-content-save-cog',
+					subLinks: [
+						{ title: 'Transaksi Kedai', to: '/transaksi-kedai', icon: 'mdi-store' },
+						{ title: 'Transaksi Pencucian', to: '/transaksi-pencucian', icon: 'mdi-car-wash' },
+					],
+				},
+				{
+					title: 'Pengaturan Data',
+					icon: 'mdi-database-cog',
+					subLinks: [
+						{ title: 'Jabatan', to: '/jabatan', icon: 'mdi-badge-account-horizontal-outline' },
+						{ title: 'Karyawan', to: '/karyawan', icon: 'mdi-account-group' },
+						{ title: 'Jenis Kendaraan', to: '/jenis-kendaraan', icon: 'mdi-car-info' },
+						{ title: 'Kendaraan', to: '/kendaraan', icon: 'mdi-car' },
+						{ title: 'Mobil Pelanggan', to: '/mobil-pelanggan', icon: 'mdi-account-key' },
+						{ title: 'Menu Kedai', to: '/menu-kedai', icon: 'mdi-book-open' },
+					],
+				},
+				{
+					title: 'Administrasi Keuangan',
+					icon: 'mdi-account-cash',
+					subLinks: [
+						{ title: 'Peminjaman Karyawan', to: '/peminjaman-karyawan', icon: 'mdi-checkbook' },
+						{ title: 'Gaji Karyawan', to: '/gaji-karyawan', icon: 'mdi-cash' },
+						{ title: 'Pengeluaran Kedai', to: '/pengeluaran-kedai', icon: 'mdi-cart-plus' },
+						{ title: 'Pembelanjaan Harian', to: '/pembelanjaan-harian', icon: 'mdi-cart-variant' },
+					],
+				},
 				{ title: 'Laporan', to: '/laporan', icon: 'mdi-file-document-outline' },
 			],
 		};
